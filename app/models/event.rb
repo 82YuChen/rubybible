ba29d9a7b8454e21d9d5748739493065d8420fe7
aegon <z851039@gmail.com>
2023-02-19 17:23:19 +0800

    ORM test

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   validates :name, presence: true
+
+  has_many :attendees
+  has_many :event_groupships
+  has_many :groups, :through => :event_groupships
+  has_one :location
+  belongs_to :category
 end
